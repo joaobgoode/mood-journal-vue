0d858a7413ec561e4a782336e38744a9e7588b5a
João Victor <joaovictorvvm@edu.unifor.br>
2025-05-24 11:15:05 -0300

    removed unnused bootstrap-icons

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import "bootstrap-icons/font/bootstrap-icons.css";
 import router from './router'
 
 createApp(App).use(router).mount('#app')
